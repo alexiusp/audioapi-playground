c893e25b895b1ad28b9995ac130a6476a7758a1a
Aleksei AlexiuS Podgaev <podgaev@gmail.com>
2018-11-03 12:48:58 +0100

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid, Navbar, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Grid fluid={true}>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Glyphicon glyph="music" />
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+
+        <div className="App">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </div>
+      </Grid>
     );
   }
 }
